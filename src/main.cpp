8c06defb160fb79ef32da2767f188e5630649265
NiceGuy909 <neilbhbob@gmail.com>
2026-08-25 02:37:53 +0530

    feat: implement repository scanner and file analysis

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <filesystem>
 #include <vector>
+#include <fstream>
+#include <optional>
 namespace fs = std::filesystem;
 
 bool shouldSkipDirectory(const fs::path& it){
@@ -34,6 +36,43 @@ std::vector<fs::path> scanRepo(fs::path path){
     return filesFound;
 }
 
+bool isTextFile(const fs::path& path){
+    std::ifstream f(path, std::ios::binary);
+    if (!f.is_open())
+        return false;
+    char c;
+    while (f.get(c)) {
+        if(!c) return false;
+    }
+    return true;
+}
+
+std::optional<int> fileLineCount(const fs::path& path){
+    if(!isTextFile(path)) return std::nullopt;
+    int lineCnt = 0;
+    std::ifstream f(path);
+    if(!f.is_open()) return std::nullopt;
+    std::string line;
+    while(std::getline(f,line)){
+        lineCnt++;
+    }
+    return lineCnt;
+}
+
+std::optional<std::string> readFile(const fs::path& path){
+    if(!isTextFile(path)) return std::nullopt;
+    std::ifstream f(path);
+    if(!f.is_open()) return std::nullopt;
+    std::string content(
+    (std::istreambuf_iterator<char>(f)),
+    std::istreambuf_iterator<char>()
+    );
+    return content;
+    
+}
+
+
+
 int main(int argc,char** argv) {
     if(argc<2){
         std::cout<<"Error: Specify Path to Repo"<<std::endl;
@@ -45,7 +84,24 @@ int main(int argc,char** argv) {
     try{
         auto files = scanRepo(path);
         for(const auto& it: files){
-            std::cout<<it<<std::endl;
+            
+            std::cout<<"Path: "<<it<<std::endl;
+            std::cout<<"Extention: "<<it.extension()<<std::endl;
+            std::cout<<"Size: "<<fs::file_size(it)<<" Bytes"<<std::endl;
+            auto count = fileLineCount(it);
+
+            if (count) {
+                std::cout << "Line Count: " << *count << std::endl;
+            }
+            
+            std::cout<<"text/binary: "<<isTextFile(it)<<std::endl;
+
+            auto content = readFile(it);
+
+            if(content){
+                std::cout<<"==== "<<it<<" ===="<<std::endl;
+                std::cout<<content.value()<<std::endl;
+            }
         }
         std::cout<<"Files Found: "<<files.size()<<std::endl;
     }
